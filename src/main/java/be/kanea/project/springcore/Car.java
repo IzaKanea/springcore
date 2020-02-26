240e5dd6dff3d57291f3dfa8ae68e29b25da32f3
IzaKanea <60557101+IzaKanea@users.noreply.github.com>
2020-02-26 15:59:34 +0100

    add picture and class

--- src/main/java/be/kanea/project/springcore/Car.java
+++ src/main/java/be/kanea/project/springcore/Car.java
@@ -1,7 +1,5 @@
 package be.kanea.project.springcore;
 
-import java.util.logging.Logger;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
